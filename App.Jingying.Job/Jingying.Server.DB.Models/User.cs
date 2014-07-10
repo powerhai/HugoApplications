31fa0673d44ba9c9e11f57938be166f14d869b8d
powerhai <12568901@qq.com>
2014-07-10 10:45:52 +0800

    Add win phone by jingling

--- App.Jingying.Job/Jingying.Server.DB.Models/User.cs
+++ App.Jingying.Job/Jingying.Server.DB.Models/User.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Sagua.Global.Domain;
-
+ 
 namespace Sagua.Server.DB.Models
 {
 	public class User
@@ -8,8 +7,7 @@ namespace Sagua.Server.DB.Models
 		[Key]
 		public long UserId { get; set; }
 		public string Name { get; set; }
-		public string Tel { get; set; }  
-		public SexType Sex { get; set; }  
+		public string Tel { get; set; }   
 		public string Address { get; set; }
 		public string MobileNumber { get; set; }
 //
