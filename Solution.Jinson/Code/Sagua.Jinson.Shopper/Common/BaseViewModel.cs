fe529a20c55ec6d916b2633b57cfe8f8df02dbc8
powerhai <12568901@qq.com>
2014-07-22 21:58:31 +0800

    adds

---
+++ Solution.Jinson/Code/Sagua.Jinson.Shopper/Common/BaseViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Media;
+using Microsoft.Practices.Unity;
+using Sagua.Global.Common;
+using Sagua.Jinson.Shopper.Controls;
+
+namespace Sagua.Jinson.Shopper.Common
+{
+    public abstract  class BaseViewModel : ObservableObject
+    {
+        protected readonly IUnityContainer mContainer;
+        private readonly RegionBusyBox mRegionBusyBox;
+        public void ShowBusyBox ()
+        {
+            mRegionBusyBox.Show();
+        }
+        public void CloseBusyBox ()
+        {
+            mRegionBusyBox.Close();
+        }
+        public    BaseViewModel (IUnityContainer container)
+        {
+            mContainer = container;
+            mRegionBusyBox = mContainer.Resolve<RegionBusyBox>();
+             
+        }
+        private bool IsLoaded = false;
+        public void Init ()
+        {
+            if(IsLoaded)
+                return;
+            LoadViewData();
+            IsLoaded = true;
+
+        }
+        public abstract    void LoadViewData ();
+    }
+}
---
+++ Solution.Jinson/Code/Sagua.Jinson.Shopper/Modules/Login/LoginViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using Sagua.Jinson.Shopper.Common;
+using Sagua.Jinson.Shopper.Controllers;
+using Sagua.Jinson.Shopper.Domain;
+using Sagua.Jinson.Shopper.Modules.Mission;
+using Sagua.Jinson.Shopper.Modules.Register;
+using Sagua.Jinson.Shopper.Services;
+
+namespace Sagua.Jinson.Shopper.Modules.Login
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+        private String mPassword;
+        public String Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+                OnPropertyChanged();
+            }
+        }
+      
+
+        private String mUserName;
+        public String UserName
+        {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+                OnPropertyChanged();
+            }
+        }
+      
+        private readonly IRegionManager mRegionManager;
+        private readonly LoginController mLoginController;
+        public LoginViewModel(IUnityContainer container, IRegionManager regionManager, LoginController loginController)
+            : base(container)
+        {
+            mRegionManager = regionManager;
+            mLoginController = loginController;
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new DelegateCommand(async () =>
+                {
+                    ShowBusyBox();
+                    var result = await  mLoginController.Login(UserName, Password );
+                    if(!result.IsOk)
+                    {
+                        MessageBox.Show( result.ErrorMessage,"登录错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    } else
+                    {
+                        mRegionManager.Regions[RegionNames.WORK_SPACE_REGION ].RequestNavigate(typeof(MissionWorkSpaceView).FullName);
+                    }
+                    CloseBusyBox();
+                });
+            }
+
+        }
+
+        
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    mRegionManager.Regions[RegionNames.WORK_SPACE_REGION].RequestNavigate(typeof(RegisterView).FullName);
+
+                });
+            }
+
+        }
+
+        public override void LoadViewData ()
+        {
+             
+        }
+    }
+}
